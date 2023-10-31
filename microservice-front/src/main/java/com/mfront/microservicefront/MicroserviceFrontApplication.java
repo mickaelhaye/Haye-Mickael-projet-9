4ea8ac84ac83a92af2d91abdd9f719627b409931
mac-m2-mickael <mickael.haye@hotmail.fr>
2023-10-31 09:27:41 +0100

    création du microservice front

---
+++ microservice-front/src/main/java/com/mfront/microservicefront/MicroserviceFrontApplication.java
@@ -0,0 +1,13 @@
+package com.mfront.microservicefront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceFrontApplication.class, args);
+    }
+
+}
---
+++ microservice-front/src/test/java/com/mfront/microservicefront/MicroserviceFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfront.microservicefront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
