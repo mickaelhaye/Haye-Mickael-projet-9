676316ad94ec97416e79dc8ec58889ffef6ae59e
mac-m2-mickael <mickael.haye@hotmail.fr>
2023-10-31 15:05:44 +0100

    microservice Gateway: Javadoc

--- microservice-gateway/src/main/java/com/mgateway/microservicegateway/MicroserviceGatewayApplication.java
+++ microservice-gateway/src/main/java/com/mgateway/microservicegateway/MicroserviceGatewayApplication.java
@@ -1,13 +1,31 @@
+/**
+ * Package principal du microservice gateway.
+ */
 package com.mgateway.microservicegateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Classe principale de démarrage de l'application Microservice Gateway.
+ * <p>
+ * Cette classe est le point d'entrée pour l'exécution du microservice.
+ * Elle utilise le mécanisme de démarrage de Spring Boot pour initialiser
+ * et exécuter l'application.
+ * </p>
+ *
+ * @author mickael hayé
+ * @version 1.0
+ */
 @SpringBootApplication
 public class MicroserviceGatewayApplication {
 
+    /**
+     * La méthode principale qui déclenche l'exécution de l'application.
+     *
+     * @param args Les arguments de la ligne de commande. Non utilisés dans cette application.
+     */
     public static void main(String[] args) {
         SpringApplication.run(MicroserviceGatewayApplication.class, args);
     }
-
 }
--- microservice-gateway/src/main/java/com/mgateway/microservicegateway/configuration/SecurityConfiguration.java
+++ microservice-gateway/src/main/java/com/mgateway/microservicegateway/configuration/SecurityConfiguration.java
@@ -1,3 +1,6 @@
+/**
+ * Package contenant les configurations pour le microservice gateway.
+ */
 package com.mgateway.microservicegateway.configuration;
 
 import org.springframework.context.annotation.Bean;
@@ -11,10 +14,27 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+/**
+ * Configuration de sécurité pour le microservice gateway.
+ * <p>
+ * Cette classe configure la sécurité pour le microservice en utilisant
+ * Spring WebFlux Security. Elle définit les règles d'accès, les utilisateurs
+ * et leurs rôles, ainsi que le mécanisme d'authentification.
+ * </p>
+ *
+ * @author mickael hayé
+ * @version 1.0
+ */
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
 
+    /**
+     * Configure les règles d'accès pour les différentes URL de l'application.
+     *
+     * @param http Une instance de {@link ServerHttpSecurity} utilisée pour configurer la sécurité.
+     * @return La chaîne de filtres de sécurité configurée.
+     */
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http.authorizeExchange()
@@ -25,10 +45,14 @@ public class SecurityConfiguration {
                 .httpBasic()
                 .and()
                 .csrf().disable();
-
         return http.build();
     }
 
+    /**
+     * Configure les détails de l'utilisateur pour l'authentification.
+     *
+     * @return Une instance de {@link MapReactiveUserDetailsService} avec les détails de l'utilisateur.
+     */
     @Bean
     public MapReactiveUserDetailsService userDetailsService() {
         UserDetails user = User.builder()
@@ -39,6 +63,11 @@ public class SecurityConfiguration {
         return new MapReactiveUserDetailsService(user);
     }
 
+    /**
+     * Fournit une instance de l'encodeur de mot de passe pour la sécurité.
+     *
+     * @return Une instance de {@link BCryptPasswordEncoder}.
+     */
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
