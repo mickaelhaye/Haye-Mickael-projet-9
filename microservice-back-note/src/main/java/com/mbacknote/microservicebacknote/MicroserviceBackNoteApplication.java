373e2bf36310fa15d6aedce5eac548d62fcfd1c1
mac-m2-mickael <mickael.haye@hotmail.fr>
2023-11-16 11:36:11 +0100

    microservice Back Note: Création du microservice

---
+++ microservice-back-note/src/main/java/com/mbacknote/microservicebacknote/MicroserviceBackNoteApplication.java
@@ -0,0 +1,13 @@
+package com.mbacknote.microservicebacknote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBackNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceBackNoteApplication.class, args);
+    }
+
+}
