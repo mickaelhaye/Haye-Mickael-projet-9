0002b68570613ce671b810269fcd469eb87314ad
mac-m2-mickael <mickael.haye@hotmail.fr>
2023-11-20 15:34:43 +0100

    microoservice Back Diabete: Création du microservice back diabète

---
+++ microservice-back-diabete/src/main/java/com/mbackdiabete/microservicebackdiabete/MicroserviceBackDiabeteApplication.java
@@ -0,0 +1,13 @@
+package com.mbackdiabete.microservicebackdiabete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBackDiabeteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceBackDiabeteApplication.class, args);
+    }
+
+}
---
+++ microservice-back-diabete/src/test/java/com/mbackdiabete/microservicebackdiabete/MicroserviceBackDiabeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbackdiabete.microservicebackdiabete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBackDiabeteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
