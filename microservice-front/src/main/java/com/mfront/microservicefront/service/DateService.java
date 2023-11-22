ca0bcec7c5ec5202f7975667ab0200f85bfd2b4b
mac-m2-mickael <mickael.haye@hotmail.fr>
2023-11-22 15:50:06 +0100

    microservice front: Rajout de Javadoc sur la classe DateService

--- microservice-front/src/main/java/com/mfront/microservicefront/service/DateService.java
+++ microservice-front/src/main/java/com/mfront/microservicefront/service/DateService.java
@@ -1,5 +1,11 @@
 package com.mfront.microservicefront.service;
 
 public interface DateService {
+    /**
+     * Fournit la date actuelle dans un format spécifié.
+     * Cette méthode est conçue pour renvoyer la date du jour selon un formatage standard,
+     *
+     * @return Une chaîne de caractères représentant la date actuelle formatée.
+     */
     String dateDuJour();
 }
--- microservice-front/src/main/java/com/mfront/microservicefront/service/impl/DateServiceImpl.java
+++ microservice-front/src/main/java/com/mfront/microservicefront/service/impl/DateServiceImpl.java
@@ -7,8 +7,23 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+
+/**
+ * Implémentation du service DateService.
+ * Cette classe fournit des fonctionnalités liées à la manipulation et au formatage des dates.
+ * Actuellement, elle propose une méthode pour obtenir la date actuelle formatée selon un schéma spécifique.
+ *
+ * @author mickael hayé
+ * @version 1.0
+ */
 @Service
 public class DateServiceImpl implements DateService {
+    /**
+     * Récupère la date actuelle et la retourne formatée en chaîne de caractères.
+     * Le format utilisé est "yyyy-MM-dd".
+     *
+     * @return String représentant la date actuelle dans le format "yyyy-MM-dd".
+     */
     @Override
     public String dateDuJour() {
         // récupération date actuelle
