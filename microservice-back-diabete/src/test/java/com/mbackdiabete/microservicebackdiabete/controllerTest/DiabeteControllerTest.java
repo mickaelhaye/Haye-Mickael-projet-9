bc11672821e96916c9e3b1cb9777caf3c950897d
mac-m2-mickael <mickael.haye@hotmail.fr>
2023-11-30 09:36:01 +0100

    Microservice diabete: Finalisation des tests

--- microservice-back-diabete/src/test/java/com/mbackdiabete/microservicebackdiabete/controllerTest/DiabeteControllerTest.java
+++ microservice-back-diabete/src/test/java/com/mbackdiabete/microservicebackdiabete/controllerTest/DiabeteControllerTest.java
@@ -1,6 +1,5 @@
 package com.mbackdiabete.microservicebackdiabete.controllerTest;
 
-
 import com.mbackdiabete.microservicebackdiabete.model.dto.PatientInfoDTO;
 import com.mbackdiabete.microservicebackdiabete.service.CalculService;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,49 +13,57 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+/**
+ * Classe de test pour DiabeteController.
+ * Utilise les outils de test de Spring Boot pour réaliser des tests d'intégration.
+ *
+ * @author Mickael Hayé
+ * @version 1.0
+ */
 @SpringBootTest
 @AutoConfigureMockMvc
 public class DiabeteControllerTest {
-    private SecurityProperties.User userTestADMIN = new SecurityProperties.User();
+    /**
+     * Un utilisateur de test pour les besoins d'authentification.
+     */
+    private final SecurityProperties.User userTest = new SecurityProperties.User();
+
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Autowired
     private CalculService calculService;
 
     @MockBean
     private RestTemplate restTemplate;
 
-
-
-
     @BeforeEach
     public void setup() {
-        userTestADMIN.setName("toto");
-        userTestADMIN.setPassword("310877");
+        userTest.setName("user1");
     }
 
+    /**
+     * Teste la méthode 'risqueDiabete' du contrôleur.
+     * Simule les appels RestTemplate et vérifie la réponse du contrôleur.
+     *
+     * @throws Exception si le test échoue
+     */
     @Test
     public void risqueDiabete() throws Exception {
         //Mock de restTemplate.exchange(url, HttpMethod.GET, entity, PatientInfoDTO.class);
@@ -68,26 +75,14 @@ public class DiabeteControllerTest {
         PatientInfoDTO patientInfoDTO = new PatientInfoDTO();
         patientInfoDTO.setDateDeNaissance(dateNaissance);
         patientInfoDTO.setGenre("H");
-        when(restTemplate.exchange(
-                anyString(),
-                eq(HttpMethod.GET),
-                any(HttpEntity.class),
-                eq(PatientInfoDTO.class)))
-                .thenReturn(new ResponseEntity<>(patientInfoDTO, HttpStatus.OK));
+        when(restTemplate.exchange(anyString(), eq(HttpMethod.GET), any(HttpEntity.class), eq(PatientInfoDTO.class))).thenReturn(new ResponseEntity<>(patientInfoDTO, HttpStatus.OK));
 
         //Mock de restTemplate.exchange(url, HttpMethod.GET, entity, String[].class);
         String[] experesponseNotes = {"anormal", "fumeur", "poids"};
-        when(restTemplate.exchange(
-                anyString(),
-                eq(HttpMethod.GET),
-                any(HttpEntity.class),
-                eq(String[].class)))
-                .thenReturn(new ResponseEntity<>(experesponseNotes, HttpStatus.OK));
+        when(restTemplate.exchange(anyString(), eq(HttpMethod.GET), any(HttpEntity.class), eq(String[].class))).thenReturn(new ResponseEntity<>(experesponseNotes, HttpStatus.OK));
 
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/diabeteBack/risque/1234").with(user("toto")).header("Authorization", ""))
-                .andExpect(status().isOk())
-               ;
+        mockMvc.perform(get("/diabeteBack/risque/1234").with(user("user1")).header("Authorization", "")).andExpect(status().isOk()).andExpect(MockMvcResultMatchers.content().string(containsString("Borderline"))).andDo(print());
 
     }
 }
--- microservice-back-diabete/src/test/java/com/mbackdiabete/microservicebackdiabete/serviceTest/CalculServiceTest.java
+++ microservice-back-diabete/src/test/java/com/mbackdiabete/microservicebackdiabete/serviceTest/CalculServiceTest.java
@@ -2,24 +2,30 @@ package com.mbackdiabete.microservicebackdiabete.serviceTest;
 
 import com.mbackdiabete.microservicebackdiabete.service.CalculService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.when;
 
+/**
+ * Classe de test pour CalculService.
+ * Effectue des tests unitaires sur les méthodes du service de calcul.
+ *
+ * @author Mickael Hayé
+ * @version 1.0
+ */
 @SpringBootTest
-@ExtendWith(MockitoExtension.class)
 public class CalculServiceTest {
 
     @Autowired
     private CalculService calculService;
+
+    /**
+     * Teste la méthode getNiveauxRisque pour les patients de plus de 30 ans.
+     * Vérifie les différents niveaux de risque basés sur les notes des patients.
+     */
     @Test
     void getNiveauxRisquePlus30ansTest() {
         //test avec la date actuelle -31ans
@@ -30,7 +36,7 @@ public class CalculServiceTest {
         List<String> notes = new ArrayList<String>();
         notes.add("anormal");
         notes.add("fumeur");
-        assertEquals("Borderline", calculService.getNiveauxRisque(dateNaissance, "F",notes));
+        assertEquals("Borderline", calculService.getNiveauxRisque(dateNaissance, "F", notes));
 
         notes.clear();
         notes.add("anormal");
@@ -39,7 +45,7 @@ public class CalculServiceTest {
         notes.add("Poids");
         notes.add("Cholestérol");
         notes.add("Rechute");
-        assertEquals("In Danger", calculService.getNiveauxRisque(dateNaissance, "F",notes));
+        assertEquals("In Danger", calculService.getNiveauxRisque(dateNaissance, "F", notes));
 
         notes.clear();
         notes.add("Hémoglobine A1C");
@@ -50,9 +56,13 @@ public class CalculServiceTest {
         notes.add("Anormal");
         notes.add("Cholestérol");
         notes.add("Vertiges");
-        assertEquals("Early onset", calculService.getNiveauxRisque(dateNaissance, "F",notes));
+        assertEquals("Early onset", calculService.getNiveauxRisque(dateNaissance, "F", notes));
     }
 
+    /**
+     * Teste la méthode getNiveauxRisque pour les patients de moins de 30 ans.
+     * Vérifie les différents niveaux de risque basés sur les notes des patients.
+     */
     @Test
     void getNiveauxRisqueMoins30ansTest() {
         //test avec la date actuelle -29ans
@@ -64,7 +74,7 @@ public class CalculServiceTest {
         notes.add("anormal");
         notes.add("fumeur");
         notes.add("Taille");
-        assertEquals("In Danger", calculService.getNiveauxRisque(dateNaissance, "H",notes));
+        assertEquals("In Danger", calculService.getNiveauxRisque(dateNaissance, "H", notes));
 
         notes.clear();
         notes.add("Hémoglobine A1C");
@@ -72,14 +82,14 @@ public class CalculServiceTest {
         notes.add("Taille");
         notes.add("Poids");
         notes.add("Fumeur");
-        assertEquals("Early onset", calculService.getNiveauxRisque(dateNaissance, "H",notes));
+        assertEquals("Early onset", calculService.getNiveauxRisque(dateNaissance, "H", notes));
 
         notes.clear();
         notes.add("Hémoglobine A1C");
         notes.add("Microalbumine");
         notes.add("Taille");
         notes.add("Poids");
-        assertEquals("In Danger", calculService.getNiveauxRisque(dateNaissance, "F",notes));
+        assertEquals("In Danger", calculService.getNiveauxRisque(dateNaissance, "F", notes));
 
         notes.clear();
         notes.add("Hémoglobine A1C");
@@ -89,23 +99,32 @@ public class CalculServiceTest {
         notes.add("Fumeur");
         notes.add("Cholestérol");
         notes.add("Vertiges");
-        assertEquals("Early onset", calculService.getNiveauxRisque(dateNaissance, "F",notes));
+        assertEquals("Early onset", calculService.getNiveauxRisque(dateNaissance, "F", notes));
 
 
     }
 
+    /**
+     * Teste la méthode calculNbrTermesDeclencheurs.
+     * Calcule le nombre de termes déclencheurs dans les notes des patients.
+     */
     @Test
     void calculNbrTermesDeclencheursTest() {
         List<String> notes = new ArrayList<String>();
         notes.add("Le patient déclare qu'il fume depuis peu");
         notes.add("Le patient déclare qu'il est fumeur et qu'il a cessé de fumer l'année dernière Il se plaint également de crises d’apnée respiratoire anormales Tests de laboratoire indiquant un taux de cholestérol LDL élevé");
-        assertEquals(3, calculService.calculNbrTermesDeclencheurs( notes));
+        assertEquals(3, calculService.calculNbrTermesDeclencheurs(notes));
 
         notes.clear();
         notes.add("LE PATIENT DÉCLARE QU'IL FUME DEPUIS PEU");
         notes.add("LE PATIENT DÉCLARE QU'IL EST FUMEUR ET QU'IL A CESSÉ DE FUMER L'ANNÉE DERNIÈRE. IL SE PLAINT ÉGALEMENT DE CRISES D’APNÉE RESPIRATOIRE ANORMALES. TESTS DE LABORATOIRE INDIQUANT UN TAUX DE CHOLESTÉROL LDL ÉLEVÉ");
-        assertEquals(3, calculService.calculNbrTermesDeclencheurs( notes));
+        assertEquals(3, calculService.calculNbrTermesDeclencheurs(notes));
     }
+
+    /**
+     * Teste la méthode calculAge.
+     * Calcule l'âge du patient à partir de sa date de naissance.
+     */
     @Test
     void calculAgeTest() {
         //test avec la date actuelle -2ans
